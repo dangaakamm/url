2d41d0a317ad3c081b48bdca2766d944e4388169
dangaakamm <836599601@qq.com>
2016-11-30 08:37:11 +0800

    add

---
+++ baidu/js/baidu.js
@@ -0,0 +1,34 @@
+$(function () {
+    var ch=$(window).height();
+    var num=0;
+    var flag=true;
+    touch.on("body","swipeup","#fullpage",function (e) {
+
+        if(!flag){
+            return;
+        }
+        num++;
+        if(num==$("section").length){
+            num=$("section").length-1;
+            return;
+        }
+        flag=false;
+        $("#fullpage").css({marginTop:-num * ch})
+    })
+    touch.on("body","swipedown","#fullpage",function (e) {
+
+        if(!flag){
+            return;
+        }
+        num--;
+        if(num==-1){
+            num=0;
+            return;
+        }
+        flag=false;
+        $("#fullpage").css({marginTop:-num * ch});
+    })
+    $("#fullpage")[0].addEventListener("webkitTransitionEnd",function () {
+        flag=true;
+    })
+})
